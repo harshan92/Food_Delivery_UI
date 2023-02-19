707e565924137b2fcd7d7809f89e45651d623e68
HM2-WIMALASIRI <harshanmadhurangaw@gmail.com>
2023-02-19 23:14:52 +0530

    Added navigations to restaurants

---
+++ lib/screens/restaurant_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:food_delevery_app/models/restaurant.dart';
+
+class RestaurantScreen extends StatefulWidget {
+  final Restaurant restaurant;
+  const RestaurantScreen({super.key, required this.restaurant});
+
+  @override
+  State<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends State<RestaurantScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold();
+  }
+}
